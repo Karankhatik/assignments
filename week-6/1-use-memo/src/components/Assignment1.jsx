96b03eb85f779ee7b146c17f1ad81b447d6d7b1d
karankhatik <karan13699@gmail.com>
2024-08-07 18:35:50 +0530

    Feat: completed week six 6 assignment

--- week-6/1-use-memo/src/components/Assignment1.jsx
+++ week-6/1-use-memo/src/components/Assignment1.jsx
@@ -1,22 +1,38 @@
-import { useState } from "react";
+import { useState, useMemo } from "react";
 
 // In this assignment, your task is to create a component that performs an expensive calculation (finding the factorial) based on a user input. 
 // Use useMemo to ensure that the calculation is only recomputed when the input changes, not on every render.
 
 export function Assignment1() {
     const [input, setInput] = useState(0);
+    const [count, setCount] = useState(0);
     // Your solution starts here
-    const expensiveValue = 0; 
+    const factorialFunction = (num) => {
+        let result = 1;
+        while(num) {
+            result *= num;
+            num -= 1;
+        }
+
+        return result;
+    }
+    const expensiveValue = useMemo(() => {
+        console.log("Calculating...")
+        return factorialFunction(input);
+
+    }, [input])
+    console.log("render component")
+    
     // Your solution ends here
 
     return (
         <div>
-            <input 
-                type="number" 
+            <input                
                 value={input} 
                 onChange={(e) => setInput(Number(e.target.value))} 
             />
             <p>Calculated Value: {expensiveValue}</p>
+            <button onClick={() => setCount(count + 1)}>Count {count}</button>
         </div>
     );
 }
--- week-6/2-use-callback/src/components/Assignment1.jsx
+++ week-6/2-use-callback/src/components/Assignment1.jsx
@@ -1,31 +1,45 @@
-import { useState } from "react";
+/* eslint-disable react/display-name */
+import { useState, useCallback, memo } from "react";
 
 // Create a counter component with increment and decrement functions. Pass these functions to a child component which has buttons to perform the increment and decrement actions. Use useCallback to ensure that these functions are not recreated on every render.
 
 export function Assignment1() {
-    const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
+  const [isCounting, setIsCounting] = useState(false);
 
-    // Your code starts here
-    function handleIncrement() {
+  // Your code starts here
+  const handleIncrement = useCallback(() => {
+    console.log("handleIncrement created");
+    setCount(count + 1);
+  }, [count]);
 
-    }
+  const handleDecrement = useCallback(() => {
+    console.log("handledecrement created");
+    setCount(count - 1);
+  }, [count]);
 
-    function handleDecrement() {
-        
-    }
-    // Your code ends here
+  // Your code ends here
 
-    return (
-        <div>
-            <p>Count: {count}</p>
-            <CounterButtons onIncrement={handleIncrement} onDecrement={handleDecrement} />
-        </div>
-    );
-};
-
-const CounterButtons = ({ onIncrement, onDecrement }) => (
+  return (
     <div>
-        <button onClick={onIncrement}>Increment</button>
-        <button onClick={onDecrement}>Decrement</button>
+      <p>Count: {count}</p>
+      <button onClick={() => setIsCounting(!isCounting)}>counting</button>
+        {isCounting ? "Stop Counting" : "Start Counting"}
+      <CounterButtons
+        onIncrement={handleIncrement}
+        onDecrement={handleDecrement}
+      />
     </div>
-);
+  );
+}
+
+// eslint-disable-next-line react/prop-types
+const CounterButtons = memo(({ onIncrement, onDecrement }) => {
+    console.log("CounterButtons created");
+  return (
+    <div>
+      <button onClick={onIncrement}>Increment</button>
+      <button onClick={onDecrement}>Decrement</button>
+    </div>
+  );
+});
